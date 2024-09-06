79a1225d1c6c0f46d402226891ae59eb96faed5c
Eduardo <71660201+Eduardo98c@users.noreply.github.com>
2024-09-06 13:31:53 +0200

    Skipfish report loaded

---
+++ Potato_files/porta_80/output_skipfish/child_index.js
@@ -0,0 +1,3 @@
+var child = [
+  { 'dupe': false, 'type': 2, 'name': 'http://10.0.2.11/', 'dir': 'c0', 'linked': 2, 'url': 'http://10.0.2.11/', 'fetched': true, 'code': 200, 'len': 245, 'decl_mime': 'text/html', 'sniff_mime': 'text/html', 'cset': 'UTF-8', 'missing': false, 'csens': true, 'child_cnt': 9, 'issue_cnt': [ 5, 0, 0, 0, 0 ], 'sig': 0xb4568314 }
+];
---
+++ Potato_files/porta_80/output_skipfish/summary.js
@@ -0,0 +1,4 @@
+var sf_version = '2.10b';
+var scan_date  = 'Fri May 10 22:28:55 2024';
+var scan_seed  = '0x83874710';
+var scan_ms    = 586;
