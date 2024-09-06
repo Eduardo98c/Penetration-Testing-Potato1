15337d7e9c5ee01bb2315090c5d1680b839bcb35
Eduardo <71660201+Eduardo98c@users.noreply.github.com>
2024-09-06 13:29:54 +0200

    Skipfish report loaded

---
+++ Potato_files/porta_80/output_skipfish/c0/c0/child_index.js
@@ -0,0 +1,6 @@
+var child = [
+  { 'dupe': false, 'type': 8, 'name': 'a.gif', 'dir': 'c0', 'linked': 0, 'url': 'http://10.0.2.11/icons/a.gif', 'fetched': true, 'code': 200, 'len': 246, 'decl_mime': 'image/gif', 'sniff_mime': 'image/gif', 'cset': '[none]', 'missing': false, 'csens': false, 'child_cnt': 0, 'issue_cnt': [ 0, 0, 0, 0, 0 ], 'sig': 0xfff7ffff },
+  { 'dupe': false, 'type': 8, 'name': 'back.gif', 'dir': 'c1', 'linked': 2, 'url': 'http://10.0.2.11/icons/back.gif', 'fetched': true, 'code': 200, 'len': 216, 'decl_mime': 'image/gif', 'sniff_mime': 'image/gif', 'cset': '[none]', 'missing': false, 'csens': false, 'child_cnt': 0, 'issue_cnt': [ 0, 0, 0, 0, 0 ], 'sig': 0xfff7ffff },
+  { 'dupe': false, 'type': 8, 'name': 'blank.gif', 'dir': 'c2', 'linked': 2, 'url': 'http://10.0.2.11/icons/blank.gif', 'fetched': true, 'code': 200, 'len': 148, 'decl_mime': 'image/gif', 'sniff_mime': 'image/gif', 'cset': '[none]', 'missing': false, 'csens': false, 'child_cnt': 0, 'issue_cnt': [ 0, 0, 0, 0, 0 ], 'sig': 0xfff7ffff },
+  { 'dupe': false, 'type': 8, 'name': 'p.gif', 'dir': 'c3', 'linked': 0, 'url': 'http://10.0.2.11/icons/p.gif', 'fetched': true, 'code': 200, 'len': 237, 'decl_mime': 'image/gif', 'sniff_mime': 'image/gif', 'cset': '[none]', 'missing': false, 'csens': false, 'child_cnt': 0, 'issue_cnt': [ 0, 0, 0, 0, 0 ], 'sig': 0xfff7ffff }
+];
